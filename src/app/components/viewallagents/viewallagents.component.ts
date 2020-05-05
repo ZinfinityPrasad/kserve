408e790f4faa10f8ecbe18a96f8ebd1dfe25d479
ZinfinityPrasad <prasad.harmalkar@zinfinity.in>
2020-05-05 22:24:44 +0530

    table
    @zinfinityprasad

--- src/app/components/viewallagents/viewallagents.component.ts
+++ src/app/components/viewallagents/viewallagents.component.ts
@@ -3,8 +3,8 @@ import { Component, OnInit } from '@angular/core';
 export interface PeriodicElement {
   name: string;
   position: number;
-  weight: number;
-  symbol: string;
+  agent: number;
+  calltype: string;
 }
 
 interface Process {
@@ -13,16 +13,16 @@ interface Process {
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+  {position: 1, name: 'Hydrogen', agent: 1.0079, calltype: 'H', },
+  {position: 2, name: 'Helium', agent: 4.0026, calltype: 'He'},
+  {position: 3, name: 'Lithium', agent: 6.941, calltype: 'Li'},
+  {position: 4, name: 'Beryllium', agent: 9.0122, calltype: 'Be'},
+  {position: 5, name: 'Boron', agent: 10.811, calltype: 'B'},
+  {position: 6, name: 'Carbon', agent: 12.0107, calltype: 'C'},
+  {position: 7, name: 'Nitrogen', agent: 14.0067, calltype: 'N'},
+  {position: 8, name: 'Oxygen', agent: 15.9994, calltype: 'O'},
+  {position: 9, name: 'Fluorine', agent: 18.9984, calltype: 'F'},
+  {position: 10, name: 'Neon', agent: 20.1797, calltype: 'Ne'},
 ];
 @Component({
   selector: 'app-viewallagents',
@@ -30,9 +30,10 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./viewallagents.component.scss']
 })
 export class ViewallagentsComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['position', 'name', 'agent', 'calltype'];
   dataSource = ELEMENT_DATA;
 
+  
   process: Process[] = [
     {value: 'steak-0', viewValue: 'Steak'},
     {value: 'pizza-1', viewValue: 'Pizza'},
@@ -43,5 +44,7 @@ export class ViewallagentsComponent implements OnInit {
 
   ngOnInit() {
   }
+
+ 
 
 }
